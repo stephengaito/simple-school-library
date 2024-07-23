66c7f301001c4182b596f9d078fadfb8cb0ce7ee
Stephen Gaito <stephen@perceptisys.co.uk>
2024-07-23 11:38:58 +0100

    completed first pass at conversion of htmxComponents to classes

---
+++ schoolLib/htmxComponents2/layout.py
@@ -0,0 +1,55 @@
+
+# consider: Accessible page titles in a Single Page App
+# see: https://hidde.blog/accessible-page-titles-in-a-single-page-app/
+
+from schoolLib.setup.configuration import config
+from schoolLib.htmxComponents.htmx import *
+
+class StdHeaders(HtmxBase) :
+  def collectHtml(self, htmlFragments) :
+
+    title = "Simple School Library"
+    if 'title' in config : title = config['title']
+
+    htmlFragments.append(f"""
+      <meta charset="utf-8">
+      <title>{title}</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"/>
+
+      <link rel="apple-touch-icon" sizes="192x192" href="/static/favicon/apple-touch-icon.png">
+      <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png">
+      <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png">
+      <link rel="manifest" href="/static/favicon/site.webmanifest">
+
+      <link rel="stylesheet" href="/static/css/main.css" type="text/css" />
+
+      <script src="/static/js/htmx.min.js"></script>
+      <script src="/static/js/idiomorph-ext.min.js"></script>
+    """)
+
+class StdBody(HtmxBase) :
+  def collectHtml(self, htmlFragments) :
+    htmlFragments.append("""
+      <div
+        id="initialOuterDiv"
+        hx-get="/menu/home"
+        hx-trigger="load delay:100ms"
+        hx-target="#initialOuterDiv"
+        hx-swap="outerHTML"
+      ></div>
+    """)
+
+class HtmlPage(HtmxBase) :
+  def __init__(self, headers, body, **kwargs) :
+    super().__init__(**kwargs)
+    self.headers = headers
+    self.body    = body
+
+  def collectHtml(self, htmlFragments) :
+    headers = computeComponent(headers)
+    body    = computeComponent(body)
+    htmlFragments.append('<!DOCTYPE html><html lang="en"><head>')
+    self.headers.collectHtml(htmlFragments)
+    htmlFragments.append("</head><body>")
+    self.body.collectHtml(htmlFragments)
+    htmlFragments.append("</body></html>")
---
+++ schoolLib/htmxComponents2/markdown.py
@@ -0,0 +1,40 @@
+
+import os
+
+from markdown import markdown
+
+from starlette.exceptions import HTTPException
+
+from schoolLib.setup.configuration import config
+from schoolLib.htmxComponents.htmx import *
+
+def loadMarkdownFromFile(aMarkdownPath) :
+  if 'markdownDir' not in config :
+    print("Markdown directory not configured")
+    raise HTTPException(404, detail="Markdown directory not configures")
+
+  markdownDir = config['markdownDir']
+
+  markdownPath = os.path.join(markdownDir, aMarkdownPath+'.md')
+  if not os.path.isfile(markdownPath) :
+    print(f"Markdown file [{markdownPath}] not found")
+    raise HTTPException(404, detail=f"Markdown file [{markdownPath}] not found")
+
+  markdownStr = ""
+  with open(markdownPath) as mdFile :
+    markdownStr = markdown(mdFile.read())
+  return markdownStr
+
+class MarkdownDiv(HtmxBase) :
+  def __init__(self, someMarkdown, **kwargs) :
+    super().__init__(**kwargs)
+    self.someMarkdown = someMarkdown
+
+  def collectHtml(self, htmlFragments) :
+    markdownHTML = markdown(someMarkdown)
+    # TODO: should this be escaped?
+    htmlFragments.append(f"""
+      <div {mdAttrs}>
+      {markdownHTML}
+      </div>
+    """)
